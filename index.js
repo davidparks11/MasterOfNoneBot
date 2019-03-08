d8a96db2da3eceb4e929475a735166e03d2dc646
David <davidparks11@gmail.com>
2019-03-08 14:51:43 -0600

    initial commit for test bot

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+const commando = require('discord.js-commando')
+const bot = new commando.CommandoClient()
+const TOKEN = 'NTUzNjEzODA3MDg4MzA0MTU5.D2QpCQ.0xHg9PsLRFX4dTQ4YqvtjB4RVMA'
+
+bot.registry.registerGroup('random', 'Random')
+bot.registry.registerDefaults()
+bot.registry.registerCommandsIn(__dirname + '/commands')
+bot.login(TOKEN)
+
+bot.on('message', (message) => {
+    if(message.content == 'ping')
+    {
+        //message.reply('This is working.')
+        message.channel.send('pong!')
+    }
+})
