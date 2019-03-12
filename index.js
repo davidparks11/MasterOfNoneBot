21f6fc80ddfcd194f8d045e11eb3bdbc4a08a047
David <davidparks11@gmail.com>
2019-03-12 01:02:16 -0500

    Change wasn't commit aparently. It's late. I should sleep.

--- index.js
+++ index.js
@@ -9,11 +9,3 @@ bot.registry.registerGroup('misc', 'Misc')
 bot.registry.registerDefaults()
 bot.registry.registerCommandsIn(__dirname + '/commands')
 bot.login(TOKEN)
-
-bot.on('message', (message) => {
-    if(message.content == 'ping')
-    {
-        //message.reply('This is working.')
-        message.channel.send('pong!')
-    }
-})
