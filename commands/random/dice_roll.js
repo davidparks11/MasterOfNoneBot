9e04da16200ac6f5fee6d6e3b23f2dbfd8f62614
David <davidparks11@gmail.com>
2019-03-20 12:24:13 -0500

    Changed Discription. Made Roll die for general die.

--- commands/random/dice_roll.js
+++ commands/random/dice_roll.js
@@ -6,20 +6,28 @@ class DiceRollCommand extends commando.Command {
             name: 'roll', 
             group: 'random',
             memberName: 'roll',
-            description: 'Rolls a die'
+            description: 'Rolls a die with a certain number of sides',
+            argsCount: 1,
+            args: [ {
+                    label: 'number of sides',
+                    key: 'sides',
+                    prompt: 'How many sides does the die have?',
+                    type: 'integer'
+                }
+            ]
             
         })
 
     }
 
-    async run(message, args) {
-      /*  if (args.isEmpty())
-        {
-            var sides = 6
-        } else {
-            args.
-        }*/
-        var roll = Math.floor(Math.random()*6) + 1
+    async run(message, { sides }) {
+        if (sides < 2) {
+            message.reply('Nice try! But we don\'t have any objects with' +
+                ' less than 2 sides to roll. We\'ll just roll a six sided' +
+                ' die!\n ')
+                sides = 6
+        }
+        var roll = Math.floor(Math.random()*sides) + 1
         message.reply('You rolled a ' + roll + '!')
     }
 }
